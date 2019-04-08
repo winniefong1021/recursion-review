0ab6479e026951fd9a14f99bf088f07d3f9f0fee
winniefong1021 <winniefong21@gmail.com>
2019-04-08 11:56:18 -0700

    Began implementing stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,44 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  
+  if (typeof obj === 'number') {
+    return '' + obj;
+  }
+  if (obj === null) {
+    return 'null';
+  }
+  if (typeof obj === 'boolean') {
+    return '' + obj;
+  }
+  if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  }
+  if (obj.constructor === Array) {
+    var output = [];
+
+    for (var i = 0; i < obj.length; i++) {
+      var ele = obj[i];
+      output.push(stringifyJSON(ele));
+    }
+    
+    return '[' + output + ']';  
+  }
+  if (obj.constructor === Object) {
+    if (obj === undefined || typeof obj === 'function') {
+      continue;
+    }
+    var output = {};
+
+    for (var key in obj) {
+      output[key] = stringifyJSON(obj[key]);
+    }  
+     
+    var string = '';
+    for (var key in output) {
+      string = string + key + output[key];
+    } 
+
+    return '{' + string '}';
+  }
 };
