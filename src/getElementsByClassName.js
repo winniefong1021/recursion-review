678fdc2eca940e8dc574dc7f1ef8929a7c3db90b
winniefong1021 <winniefong21@gmail.com>
2019-04-08 16:27:11 -0700

    Implement strategy for getElements

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,23 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+  var results = [];
+
+  var getChildren = function(node) {
+    var classes = node.classList;
+    console.log(classes); // fix this to get correct list
+    if (node.className === className) {
+      results.push(node);
+    }
+    if (node.hasChildNodes) {
+      var children = node.childNodes;
+      for (var i = 0; i < children.length; i++) {
+        getChildren(children[i]);
+      }
+    }
+  }
+
+  getChildren(document.body);
+  return results;
 };
